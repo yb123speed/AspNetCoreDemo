8e0290ae3e675d522a402230d87f8f34b53d44c8
yb123speed <904896209@qq.com>
2018-09-29 17:36:20 +0800

    init

---
+++ AuthenticationCookiesSample/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.IdentityModel.Claims;
+
+namespace AuthenticationCookiesSample.Pages
+{
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public void Post()
+        {
+            //登陆验证
+            
+            //保存cookie
+            var claims =new List<Claim>(){
+                new Claim(ClaimTypes.Name,"Yebin")
+            };
+
+            var claimsIdentity=new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+    }
+}
