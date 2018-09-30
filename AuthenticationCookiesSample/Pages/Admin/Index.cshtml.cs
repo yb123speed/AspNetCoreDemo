56ee6b3dfd476d4da067f8271791c65c646eb829
yb123speed <904896209@qq.com>
2018-09-30 17:04:16 +0800

    Razor Pages Cookie #2

---
+++ AuthenticationCookiesSample/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthenticationCookiesSample.Pages.Admin
+{
+    [Authorize(AuthenticationSchemes = "Admin")]
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
