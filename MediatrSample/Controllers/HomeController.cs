d19b173bfc94c1d660c8f98ab92ea84e13f0dc71
yb123speed <904896209@qq.com>
2018-10-02 23:54:30 +0800

    mediatR publish sample

--- MediatrSample/Controllers/HomeController.cs
+++ MediatrSample/Controllers/HomeController.cs
@@ -2,16 +2,68 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MediatrSample.Models;
+using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace MediatrSample.Controllers
 {
+    #region MediatR
+    public class SomeEvent : INotification
+    {
+        public SomeEvent(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; }
+    }
+
+    public class Handler1 : INotificationHandler<SomeEvent>
+    {
+        private readonly ILogger<Handler1> _logger;
+
+        public Handler1(ILogger<Handler1> logger)
+        {
+            _logger = logger;
+        }
+        public Task Handle(SomeEvent notification, CancellationToken cancellationToken)
+        {
+            _logger.LogWarning($"Handled: {notification.Message}");
+            return Task.CompletedTask;
+        }
+    }
+    public class Handler2 : INotificationHandler<SomeEvent>
+    {
+        private readonly ILogger<Handler2> _logger;
+
+        public Handler2(ILogger<Handler2> logger)
+        {
+            _logger = logger;
+        }
+        public Task Handle(SomeEvent notification,CancellationToken cancellationToken)
+        {
+            _logger.LogWarning($"Handled: {notification.Message}");
+            return Task.CompletedTask;
+        }
+    }
+
+    #endregion
+
     public class HomeController : Controller
     {
-        public IActionResult Index()
+            private readonly IMediator _mediator;
+
+        public HomeController(IMediator mediator)
         {
+            _mediator = mediator;
+        }
+        public async Task<IActionResult> Index()
+        {
+            await _mediator.Publish(new SomeEvent("Hello World"),CancellationToken.None);
             return View();
         }
 
