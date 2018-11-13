a1507b696bdd9ad4fb0758bbc5c33e5b58d9fb6a
yb123speed <904896209@qq.com>
2018-11-13 10:27:15 +0800

    添加验证依赖注入

--- FluentValidationSample/Controllers/ValuesController.cs
+++ FluentValidationSample/Controllers/ValuesController.cs
@@ -21,5 +21,20 @@ namespace FluentValidationSample.Controllers
                ? Ok(new { code = -1, data = new List<string>(), msg = results.Errors.FirstOrDefault().ErrorMessage })
                : Ok(new { code = 0, data = new List<string> { "v1", "v2" }, msg = "" });
         }
+
+        private readonly IValidator<QueryStudentHobbiesDto> _validator;
+        public ValuesController(IValidator<QueryStudentHobbiesDto> validator)
+        {
+            _validator = validator;
+        }
+
+        [HttpGet("hobbies2")]
+        public IActionResult GetHobbies2([FromQuery] QueryStudentHobbiesDto dto)
+        {
+            var results = _validator.Validate(dto, ruleSet: "all");
+            return !results.IsValid
+               ? Ok(new { code = -1, data = new List<string>(), msg = results.Errors.FirstOrDefault().ErrorMessage })
+               : Ok(new { code = 0, data = new List<string> { "v1", "v2" }, msg = "" });
+        }
     }
 }
--- FluentValidationSample/Startup.cs
+++ FluentValidationSample/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidationSample.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +28,8 @@ namespace FluentValidationSample
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IValidator<QueryStudentHobbiesDto>,QueryStudentHobbiesDtoValidator>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
