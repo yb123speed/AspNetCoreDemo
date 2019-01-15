d28a7b94f8b6ab3773deff468b85bc5a32f82389
yb123speed <904896209@qq.com>
2019-01-15 17:29:34 +0800

    添加Task Entity
    Signed-off-by: yb123speed <904896209@qq.com>

---
+++ ABPSamples/SimpleTaskApp/src/SimpleTaskApp.Core/Tasks/Task.cs
@@ -0,0 +1,46 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using Abp.Timing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SimpleTaskApp.Tasks
+{
+    public class Task:Entity,IHasCreationTime
+    {
+        public const int MaxTitleLength = 256;
+        public const int MaxDescriptionLength = 64 * 1024; //64kb
+
+        [Required]
+        [StringLength(MaxTitleLength)]
+        public string Title { get; set; }
+
+        [StringLength(MaxDescriptionLength)]
+        public string Description { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+        public TaskState State { get; set; }
+
+        public Task()
+        {
+            CreationTime = Clock.Now;
+            State = TaskState.Open;
+        }
+
+        public Task(string title, string description = null)
+            : this()
+        {
+            Title = title;
+            Description = description;
+        }
+    }
+
+    public enum TaskState : byte
+    {
+        Open = 0,
+        Completed = 1
+    }
+}
