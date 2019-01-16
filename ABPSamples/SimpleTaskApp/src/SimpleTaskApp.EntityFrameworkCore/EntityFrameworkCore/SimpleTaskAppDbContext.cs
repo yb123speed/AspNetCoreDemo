9de720fd135c7d5c5d65a977b58cf1e1ba802329
yb123speed <904896209@qq.com>
2019-01-16 09:10:26 +0800

    把Task添加到数据库上下文中
    Signed-off-by: yb123speed <904896209@qq.com>

--- ABPSamples/SimpleTaskApp/src/SimpleTaskApp.EntityFrameworkCore/EntityFrameworkCore/SimpleTaskAppDbContext.cs
+++ ABPSamples/SimpleTaskApp/src/SimpleTaskApp.EntityFrameworkCore/EntityFrameworkCore/SimpleTaskAppDbContext.cs
@@ -1,11 +1,13 @@
 ﻿using Abp.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SimpleTaskApp.Tasks;
 
 namespace SimpleTaskApp.EntityFrameworkCore
 {
     public class SimpleTaskAppDbContext : AbpDbContext
     {
         //Add DbSet properties for your entities...
+        public DbSet<Task> Tasks { get; set; }
 
         public SimpleTaskAppDbContext(DbContextOptions<SimpleTaskAppDbContext> options) 
             : base(options)
