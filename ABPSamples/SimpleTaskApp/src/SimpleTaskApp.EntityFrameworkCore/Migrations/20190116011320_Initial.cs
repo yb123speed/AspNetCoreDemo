eda868998667fc47fd605a49080cbd03a34384d1
yb123speed <904896209@qq.com>
2019-01-16 09:19:27 +0800

    第一次数据库迁移
    Signed-off-by: yb123speed <904896209@qq.com>

---
+++ ABPSamples/SimpleTaskApp/src/SimpleTaskApp.EntityFrameworkCore/Migrations/20190116011320_Initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SimpleTaskApp.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(maxLength: 256, nullable: false),
+                    Description = table.Column<string>(maxLength: 65536, nullable: true),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    State = table.Column<byte>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
