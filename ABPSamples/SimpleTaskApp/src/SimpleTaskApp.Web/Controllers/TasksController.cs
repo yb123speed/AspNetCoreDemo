8d11df006592b5d44ff9fa0f3e4e4e56cea3ad4a
yb123speed <904896209@qq.com>
2019-01-16 16:12:09 +0800

    Task 列表页面
    Signed-off-by: yb123speed <904896209@qq.com>

---
+++ ABPSamples/SimpleTaskApp/src/SimpleTaskApp.Web/Controllers/TasksController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleTaskApp.Tasks;
+using SimpleTaskApp.Tasks.Dtos;
+using SimpleTaskApp.Web.Models.Tasks;
+
+namespace SimpleTaskApp.Web.Controllers
+{
+    public class TasksController : SimpleTaskAppControllerBase
+    {
+        private readonly ITaskAppService _taskAppService;
+
+        public TasksController(ITaskAppService taskAppService)
+        {
+            _taskAppService = taskAppService;
+        }
+
+        public async Task<IActionResult> Index(GetAllTasksInput input)
+        {
+            var output = await _taskAppService.GetAllAsync(input);
+            var model = new IndexViewModel(output.Items);
+            return View(model);
+        }
+    }
+}
---
+++ ABPSamples/SimpleTaskApp/src/SimpleTaskApp.Web/Models/Tasks/IndexViewModel.cs
@@ -0,0 +1,30 @@
+﻿using SimpleTaskApp.Tasks;
+using SimpleTaskApp.Tasks.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleTaskApp.Web.Models.Tasks
+{
+    public class IndexViewModel
+    {
+        public IReadOnlyList<TaskListDto> Tasks { get; }
+
+        public IndexViewModel(IReadOnlyList<TaskListDto> tasks)
+        {
+            Tasks = tasks;
+        }
+
+        public string GetTaskLabel(TaskListDto task)
+        {
+            switch (task.State)
+            {
+                case TaskState.Open:
+                    return "label-success";
+                default:
+                    return "label-default";
+            }
+        }
+    }
+}
