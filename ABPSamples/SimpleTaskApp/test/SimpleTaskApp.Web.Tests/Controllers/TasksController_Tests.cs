4cb588c089e18c2b14f7fa6b85edd6f5083a54cb
yb123speed <904896209@qq.com>
2019-01-17 10:47:00 +0800

    自动化页面测试
    Signed-off-by: yb123speed <904896209@qq.com>

---
+++ ABPSamples/SimpleTaskApp/test/SimpleTaskApp.Web.Tests/Controllers/TasksController_Tests.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using SimpleTaskApp.Web.Controllers;
+using Shouldly;
+using Xunit;
+using SimpleTaskApp.Tasks;
+using Task = System.Threading.Tasks.Task;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using AngleSharp.Parser.Html;
+
+namespace SimpleTaskApp.Web.Tests.Controllers
+{
+    public class TasksController_Tests: SimpleTaskAppWebTestBase
+    {
+        [Fact]
+        public async Task Index_Test()
+        {
+            //Act
+            var response = await GetResponseAsStringAsync(
+                GetUrl<TasksController>(nameof(TasksController.Index), new
+                {
+                    state = TaskState.Open
+                })
+            );
+            
+            //Assert
+            response.ShouldNotBeNullOrWhiteSpace();
+
+            var tasksInDatabase = await UsingDbContextAsync(async context =>
+            {
+                return await context.Tasks.Where(t => t.State == TaskState.Open).ToListAsync();
+            });
+
+            var document = new HtmlParser().Parse(response);
+            var listItems = document.QuerySelectorAll("#TaskList li");
+
+            listItems.Length.ShouldBe(tasksInDatabase.Count);
+
+            foreach (var listItem in listItems)
+            {
+                var header = listItem.QuerySelector(".list-group-item-heading");
+                var taskTitle = header.InnerHtml.Trim();
+                tasksInDatabase.Any(t => t.Title == taskTitle).ShouldBeTrue();
+            }
+        }
+    }
+}
