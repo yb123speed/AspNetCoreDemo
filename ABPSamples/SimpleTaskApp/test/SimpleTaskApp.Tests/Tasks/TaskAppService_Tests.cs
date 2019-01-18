fabf21362d18473a427a7d83ee55a16c965f332b
yb123speed <904896209@qq.com>
2019-01-18 13:45:19 +0800

    Testing Task Creation Service
    Signed-off-by: yb123speed <904896209@qq.com>

--- ABPSamples/SimpleTaskApp/test/SimpleTaskApp.Tests/Tasks/TaskAppService_Tests.cs
+++ ABPSamples/SimpleTaskApp/test/SimpleTaskApp.Tests/Tasks/TaskAppService_Tests.cs
@@ -1,5 +1,7 @@
-﻿using Shouldly;
+﻿using Abp.Runtime.Validation;
+using Shouldly;
 using SimpleTaskApp.Tasks;
+using SimpleTaskApp.Tasks.Dtos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,7 +23,7 @@ namespace SimpleTaskApp.Tests.Tasks
         public async System.Threading.Tasks.Task Should_Get_All_Tasks()
         {
             //Act
-            var output = await _taskAppService.GetAllAsync(new SimpleTaskApp.Tasks.Dtos.GetAllTasksInput());
+            var output = await _taskAppService.GetAllAsync(new GetAllTasksInput());
 
             //Assert
             output.Items.Count.ShouldBe(2);
@@ -32,10 +34,56 @@ namespace SimpleTaskApp.Tests.Tasks
         public async System.Threading.Tasks.Task Should_Get_Filtered_Tasks()
         {
             //Act
-            var output = await _taskAppService.GetAllAsync(new SimpleTaskApp.Tasks.Dtos.GetAllTasksInput() { State= TaskState.Open});
+            var output = await _taskAppService.GetAllAsync(new GetAllTasksInput() { State= TaskState.Open});
 
             //Assert
             output.Items.ShouldAllBe(t => t.State == TaskState.Open);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task Should_Create_New_Task_With_Title()
+        {
+            await _taskAppService.Create(new CreateTaskInput
+            {
+                Title = "Newly created task #1"
+            });
+
+            UsingDbContext(context =>
+            {
+                var task1 = context.Tasks.FirstOrDefault(t => t.Title == "Newly created task #1");
+                task1.ShouldNotBeNull();
+            });
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task Should_Create_New_Task_With_Title_And_Assigned_Person()
+        {
+            var neo = UsingDbContext(context => context.People.Single(p => p.Name == "Neo"));
+
+            await _taskAppService.Create(new CreateTaskInput
+            {
+                Title = "Newly created task #1",
+                AssignedPersonId = neo.Id
+            });
+
+            UsingDbContext(context =>
+            {
+                var task1 = context.Tasks.FirstOrDefault(t => t.Title == "Newly created task #1");
+                task1.ShouldNotBeNull();
+                task1.AssignedPersonId.ShouldBe(neo.Id);
+            });
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task Should_Not_Create_New_Task_Without_Title()
+        {
+            await Assert.ThrowsAsync<AbpValidationException>(async () =>
+            {
+                await _taskAppService.Create(new CreateTaskInput
+                {
+                    Title = null
+                });
+            });
         }
     }
 }
