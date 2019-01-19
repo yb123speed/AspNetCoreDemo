010fb5128d67d450d77baef596a2d92b9778277a
yb123speed <904896209@qq.com>
2019-01-19 15:14:47 +0800

    Task Creation Page
    Signed-off-by: yb123speed <904896209@qq.com>

--- ABPSamples/SimpleTaskApp/src/SimpleTaskApp.Web/Controllers/TasksController.cs
+++ ABPSamples/SimpleTaskApp/src/SimpleTaskApp.Web/Controllers/TasksController.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Abp.Application.Services.Dto;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SimpleTaskApp.Common;
 using SimpleTaskApp.Tasks;
 using SimpleTaskApp.Tasks.Dtos;
+using SimpleTaskApp.Web.Models.People;
 using SimpleTaskApp.Web.Models.Tasks;
 
 namespace SimpleTaskApp.Web.Controllers
@@ -12,10 +16,14 @@ namespace SimpleTaskApp.Web.Controllers
     public class TasksController : SimpleTaskAppControllerBase
     {
         private readonly ITaskAppService _taskAppService;
+        private readonly ILookupAppService _lookupAppService;
 
-        public TasksController(ITaskAppService taskAppService)
+        public TasksController(
+            ITaskAppService taskAppService,
+            ILookupAppService  lookupAppService)
         {
             _taskAppService = taskAppService;
+            _lookupAppService = lookupAppService;
         }
 
         public async Task<IActionResult> Index(GetAllTasksInput input)
@@ -26,6 +34,17 @@ namespace SimpleTaskApp.Web.Controllers
                 SelectedTaskState = input.State
             };
             return View(model);
+        }
+
+        public async Task<ActionResult> Create()
+        {
+            var peopleSelectListItems = (await _lookupAppService.GetPeopleComboboxItemsAsync()).Items
+                .Select(p => p.ToSelectListItem())
+                .ToList();
+
+            peopleSelectListItems.Insert(0, new SelectListItem { Value = string.Empty, Text = L("Unassigned"), Selected = true });
+
+            return View(new CreateTaskViewModel(peopleSelectListItems));
         }
     }
 }
