ee27f32c2feb0db51001493dfe6ce8372d622b1f
yb123speed <904896209@qq.com>
2019-01-19 15:13:13 +0800

    LookupAppService
    Signed-off-by: yb123speed <904896209@qq.com>

---
+++ ABPSamples/SimpleTaskApp/src/SimpleTaskApp.Application/Common/ILookupAppService.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTaskApp.Common
+{
+    public interface ILookupAppService:IApplicationService
+    {
+        Task<ListResultDto<ComboboxItemDto>> GetPeopleComboboxItemsAsync();
+    }
+}
---
+++ ABPSamples/SimpleTaskApp/src/SimpleTaskApp.Application/Common/LookupAppService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Application.Services.Dto;
+using Abp.Domain.Repositories;
+using SimpleTaskApp.People;
+
+namespace SimpleTaskApp.Common
+{
+    public class LookupAppService : SimpleTaskAppAppServiceBase, ILookupAppService
+    {
+        private readonly IRepository<Person, Guid> _personRepository;
+
+        public LookupAppService(IRepository<Person, Guid> personRepository)
+        {
+            _personRepository = personRepository;
+        }
+
+        public async Task<ListResultDto<ComboboxItemDto>> GetPeopleComboboxItemsAsync()
+        {
+            var people = await _personRepository.GetAllListAsync();
+            return new ListResultDto<ComboboxItemDto>(
+                    people.Select(p =>new ComboboxItemDto(p.Id.ToString("D"), p.Name)).ToList()
+                );
+        }
+    }
+}
