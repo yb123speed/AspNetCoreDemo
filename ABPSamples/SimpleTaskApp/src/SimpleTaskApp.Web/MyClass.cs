94aa4095d8678998e11ef1c64952594fae81b86b
yb123speed <904896209@qq.com>
2019-02-01 16:42:05 +0800

    注入Session
    Signed-off-by: yb123speed <904896209@qq.com>

---
+++ ABPSamples/SimpleTaskApp/src/SimpleTaskApp.Web/MyClass.cs
@@ -0,0 +1,24 @@
+﻿using Abp.Dependency;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleTaskApp.Web
+{
+    public class MyClass:ITransientDependency
+    {
+        public IAbpSession AbpSession { get; set; }
+
+        public MyClass()
+        {
+            AbpSession = NullAbpSession.Instance;
+        }
+
+        public void MyMethod()
+        {
+            var currentUserId = AbpSession.UserId;
+            //...
+        }
+    }
+}
