00d061cd10c79343185f946945829269eca2a35b
yb123speed <904896209@qq.com>
2019-02-01 16:36:26 +0800

    依赖注入
    Signed-off-by: yb123speed <904896209@qq.com>

---
+++ ABPSamples/SimpleTaskApp/src/SimpleTaskApp.Application/People/IPersonAppService.cs
@@ -0,0 +1,11 @@
+﻿using Abp.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTaskApp.People
+{
+    public interface IPersonAppService:IApplicationService
+    {
+    }
+}
---
+++ ABPSamples/SimpleTaskApp/src/SimpleTaskApp.Application/People/PersonAppService.cs
@@ -0,0 +1,12 @@
+﻿using Abp.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTaskApp.People
+{
+    public class PersonAppService:IPersonAppService
+    {
+        
+    }
+}
