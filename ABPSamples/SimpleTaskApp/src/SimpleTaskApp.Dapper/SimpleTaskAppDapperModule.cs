b6c381a0515127cbbe45e7a9e3032804b2195960
yb123speed <904896209@qq.com>
2019-02-13 17:25:13 +0800

    初始化映射关系
    Signed-off-by: yb123speed <904896209@qq.com>

--- ABPSamples/SimpleTaskApp/src/SimpleTaskApp.Dapper/SimpleTaskAppDapperModule.cs
+++ ABPSamples/SimpleTaskApp/src/SimpleTaskApp.Dapper/SimpleTaskAppDapperModule.cs
@@ -1,19 +1,25 @@
 ﻿using Abp.Dapper;
+using Abp.EntityFrameworkCore;
 using Abp.Modules;
 using Abp.Reflection.Extensions;
 using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace SimpleTaskApp.Dapper
 {
     [DependsOn(
         typeof(SimpleTaskAppCoreModule),
-        typeof(AbpEntityFrameworkModule),
+        typeof(AbpEntityFrameworkCoreModule),
         typeof(AbpDapperModule))]
     public class SimpleTaskAppDapperModule : AbpModule
     {
         public override void Initialize()
         {
             IocManager.RegisterAssemblyByConvention(typeof(SimpleTaskAppDapperModule).GetAssembly());
+
+            //这里会自动去扫描程序集中配置好的映射关系
+            DapperExtensions.DapperExtensions.SetMappingAssemblies(new List<Assembly> { typeof(SimpleTaskAppDapperModule).GetAssembly() });
         }
     }
 }
