3e1cdc70697d1c049873377ba72cfe88191f271e
yb123speed <904896209@qq.com>
2019-02-13 17:17:46 +0800

    添加映射类
    Signed-off-by: yb123speed <904896209@qq.com>

---
+++ ABPSamples/SimpleTaskApp/src/SimpleTaskApp.Dapper/Mappers/PersonMapper.cs
@@ -0,0 +1,17 @@
+﻿using DapperExtensions.Mapper;
+using SimpleTaskApp.People;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTaskApp.Dapper.Mappers
+{
+    public class PersonMapper : ClassMapper<Person>
+    {
+        public PersonMapper()
+        {
+            Table("AppPersons");
+            AutoMap();
+        }
+    }
+}
