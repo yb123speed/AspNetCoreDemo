22f2e36b7d4f9475745a2ff1a6f09a29720b2c37
yb123speed <904896209@qq.com>
2019-02-14 17:03:59 +0800

    使用Dapper注入编写应用层服务
    Signed-off-by: yb123speed <904896209@qq.com>

--- ABPSamples/SimpleTaskApp/src/SimpleTaskApp.Application/People/PersonAppService.cs
+++ ABPSamples/SimpleTaskApp/src/SimpleTaskApp.Application/People/PersonAppService.cs
@@ -1,12 +1,28 @@
-﻿using Abp.Domain.Repositories;
+﻿using Abp.Dapper.Repositories;
+using Abp.Dependency;
+using Abp.Domain.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SimpleTaskApp.People
 {
-    public class PersonAppService:IPersonAppService
+    public class PersonAppService: ITransientDependency//,IPersonAppService
     {
-        
+        private readonly IDapperRepository<Person,Guid> _personDapperRepository;
+        private readonly IRepository<Person,Guid> _personRepository;
+
+        public PersonAppService(
+            IRepository<Person,Guid> personRepository,
+            IDapperRepository<Person,Guid> personDapperRepository)
+        {
+            _personRepository = personRepository;
+            _personDapperRepository = personDapperRepository;
+        }
+
+        public void DoSomeStuff()
+        {
+            var people = _personDapperRepository.Query("select * from Persons");
+        }
     }
 }
