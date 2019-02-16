b8793369d805fd4ce06ee2a9d418f0acf792534b
yb123speed <904896209@qq.com>
2019-02-16 16:02:47 +0800

    SQL测试
    Signed-off-by: yb123speed <904896209@qq.com>

--- ABPSamples/SimpleTaskApp/src/SimpleTaskApp.Application/People/PersonAppService.cs
+++ ABPSamples/SimpleTaskApp/src/SimpleTaskApp.Application/People/PersonAppService.cs
@@ -1,8 +1,10 @@
 ﻿using Abp.Dapper.Repositories;
 using Abp.Dependency;
 using Abp.Domain.Repositories;
+using Abp.Timing;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SimpleTaskApp.People
@@ -22,7 +24,15 @@ namespace SimpleTaskApp.People
 
         public void DoSomeStuff()
         {
+
             var people = _personDapperRepository.Query("select * from AppPersons");
+            _personDapperRepository.Execute("Insert into AppPersons (Id,Name,CreationTime) values ('" + Guid.NewGuid() + "','Name_" + Guid.NewGuid().ToString().Substring(10) + "','" + Clock.Now.ToString() + "')");
+            _personDapperRepository.Execute("UPDATE AppPersons set Name=@name where Id=@Id", new { name = "Name_" + Guid.NewGuid().ToString().Substring(10), Id = people.FirstOrDefault()?.Id });
+            //var person = people.FirstOrDefault();
+            //person.Name = "Yebin_" + Guid.NewGuid().ToString().Substring(15);
+            //_personDapperRepository.Update(person);
+            //person = _personDapperRepository.Single(people.Skip(1).Take(1).First().Id);
+            //_personDapperRepository.Insert(new Person { Name= "Yebin_" + Guid.NewGuid().ToString().Substring(15),Id= Guid.NewGuid(),CreationTime=Clock.Now});
         }
     }
 }
