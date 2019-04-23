77601289213a7b14f88248d62c3cf7b3bd35766c
yb123speed <904896209@qq.com>
2019-04-23 16:51:46 +0800

    基于Xunit的简单单元测试实现

--- XunitSamples/XunitTestDemo.Tests/Calculator_Test.cs
+++ XunitSamples/XunitTestDemo.Tests/Calculator_Test.cs
@@ -6,9 +6,16 @@ namespace XunitTestDemo.Tests
     public class Calculator_Test
     {
         [Fact]
-        public void Test1()
+        public void Add_Test()
         {
+            // Arrange
+            var sut = new Calculator(); // sut - System Under Test
 
+            // Act
+            var result = sut.Add(2, 3);
+
+            // Assert
+            Assert.Equal(expected: 5, actual: result);
         }
     }
 }
--- XunitSamples/XunitTestDemo/Calculator.cs
+++ XunitSamples/XunitTestDemo/Calculator.cs
@@ -4,5 +4,9 @@ namespace XunitTestDemo
 {
     public class Calculator
     {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
     }
 }
